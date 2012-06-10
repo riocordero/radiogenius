2a5266af4af4f9c9fc71a1bddd0251b5728fbfa1
dealmaker <jeff@bluetornadosf.comm>
2012-06-10 13:49:56 -0700

    header/footer, etc

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Radiogenius::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'search#index'
 
   # See how all your routes lay out with "rake routes"
 
