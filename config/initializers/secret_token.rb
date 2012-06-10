2361a6ca693681124b7f55a79526e5a079f4826d
James Zhang <james@bluetornadosf.com>
2012-06-10 12:10:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiogenius::Application.config.secret_token = 'a8fa100ff59e071b42f92db90dffa2926ec163d7fae3938dd41403ef0e8a0a1c811a23418e0b44ac45d6d212f3681c813cd686266c8513c764bce208da0839b9'
