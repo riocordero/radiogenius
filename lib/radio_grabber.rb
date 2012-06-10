50bdc67813e16ca6a529e7f3916b052e3a1ff8de
Hsiu-Fan Wang <hfwang@porkbuns.net>
2012-06-10 14:46:36 -0700

    We do not trust http::parser, so this monkey patch does it

--- lib/radio_grabber.rb
+++ lib/radio_grabber.rb
@@ -1,63 +1,93 @@
 # monkey patch EM::HttpConnection
+HEADER_SEPARATOR = "\r\n"
+HEADER_END_SEPARATOR = "\r\n\r\n"
+
 module EventMachine
   class HttpConnection
     def receive_data(data)
+      if !defined?(@hacked)
+        @hacked = true
+        @count = 0
+        @total_bytes = 0
+        @headers = {}
+        @header_buffer = ''
+        @headers_done = false
+      end
+      puts "Received data: #{@count += 1} #{@total_bytes += data.length}"
       begin
-        if !defined?(@hack)
-          data.gsub!("ICY", "HTTP/1.1") # hack to change ICY to HTTP/1.1 so HTTP::Parser don't freak out
-          @hack = true
-        end 
-        @p << data
+        # @p << data
+        if !@headers_done
+          idx = data.index(HEADER_END_SEPARATOR)
+          if idx
+            @header_buffer += data[0, idx]
+            data = data[(idx + HEADER_END_SEPARATOR.length)..-1]
+
+            @headers_done = true
+            @header_buffer.split(HEADER_SEPARATOR).each do |full_header|
+              key, value = full_header.split(':').map(&:strip)
+              @headers[key] = value
+            end
+            puts "Parsed headers."
+        @headers.each do |k, v|
+          puts "#{k} - #{v}"
+        end
+          else
+            @header_buffer += data
+          end
+        end
+
+        puts "\tHeaders? #{@headers_done}"
+        if @headers_done
+          @stream.call(data)
+        end
       rescue HTTP::Parser::Error => e
         c = @clients.shift
         c.nil? ? unbind(e.message) : c.on_error(e.message)
       end
-    end 
+    end
   end
 end
 
 
 class RadioGrabber
-  
   attr_reader :url
-  
+
   def initialize(url)
     @url = url
     @conn = {
       :connect_timeout => '5',
       :inactivity_timeout => '10',
-      :proxy => {
-        :host => '127.0.0.1',    # proxy address
-        :port => 8888,           # proxy port
-      },
+      # :proxy => {
+      #   :host => '127.0.0.1',    # proxy address
+      #   :port => 8888,           # proxy port
+      # },
     }
     @options = {
       :redirects => 5,           # follow 3XX redirects up to depth 5
       :keepalive => true,
       :head => {
         "Icy-MetaData" => "1",
-        # 'Accept' => '*/*'              
+        # 'Accept' => '*/*'
       }
     }
   end
-  
+
   def grab
     EM.run do
-      http = EventMachine::HttpRequest.new(url, @conn).get(@options)
-      
-      http.errback {
-        puts http.error
+      http = EventMachine::HttpRequest.new(url, @conn)
+      streamer = Proc.new { |chunk|
+        puts "Stream callback hit"
       }
+      http.instance_variable_set(:@stream, streamer)
+      http = http.get
 
       http.headers {
-        puts "icy-metaint: #{http.response_header['icy-metaint']}"
+        http.response_header.each do |k, v|
+          puts "#{k} - #{v}"
+        end
+        # puts "icy-metaint: #{http.response_header['icy-metaint']}"
       }
-
-      http.stream { |chunk|
-        puts chunk
-      }
-
     end
   end
-  
+
 end
